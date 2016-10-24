2f5082e97f85adbd7cdf1b404a3aed2e90ee9924
Siddharth <siddharthyadav123>
2016-10-24 16:45:22 +0530

    added db code

---
+++ backend/prac_doc.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Community v11.01 (32 bit)
+MySQL - 5.5.24-log : Database - prac_doc_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`prac_doc_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `prac_doc_db`;
+
+/*Table structure for table `user_table` */
+
+DROP TABLE IF EXISTS `user_table`;
+
+CREATE TABLE `user_table` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uname` (`uname`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_table` */
+
+insert  into `user_table`(`id`,`uname`,`pwd`,`address`,`full_name`,`created_on`,`updated_on`) values (1,'siddharth','12345678','ballarpur','siddharth suresh yadav','2016-10-21 16:00:59','2016-10-21 16:00:59');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
